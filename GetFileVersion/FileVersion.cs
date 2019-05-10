2dc5164764b3225db7318c871d378b54808aa54f
ChrisKolan <chris.kolan@gmail.com>
2019-05-10 20:55:08 +0200

    Add project files.

---
+++ GetFileVersion/FileVersion.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace GetFileVersion
+{
+    public class FileVersion
+    {
+        private static string _PathToFile;
+
+        public FileVersion(string path)
+        {
+            _PathToFile = path;
+        }
+
+        public static string Get()
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(_PathToFile);
+            return versionInfo.ProductVersion;
+        }
+    }
+}
